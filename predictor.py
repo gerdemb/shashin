a0096673a3de498cdb723889874851a63fbec3a4
Ben Gerdemann <github@ibeni.net>
2021-01-21 11:48:47 +0000

    New pipeline based transformer

--- predictor.py
+++ predictor.py
@@ -1,79 +1,160 @@
 import json
+from collections import defaultdict
 
 import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
-
-
-# Make a difference vector between two metadatas
-def cmp_metadata(metadata_a, metadata_b):
-    def cmp(a, b):
-        if a is not None and b is not None:
-            return (a > b) - (a < b)
-        elif a is None and b is None:
-            return 0
-        elif a is None:
-            return -1
-        elif b is None:
-            return 1
-
-    keys = set(list(metadata_a.keys()) + list(metadata_b.keys()))
-
-    feature = {}
-    for key in keys:
-        a = metadata_a.get(key)
-        b = metadata_b.get(key)
-        # Compare strings by length instead of alphabetical order
-        if isinstance(a, str): a=len(a)
-        if isinstance(b, str): b=len(b)
-        feature[key] = cmp(a,b)
-    return feature
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import FeatureUnion, Pipeline
+from sklearn.preprocessing import FunctionTransformer
 
 
 def build_predictor(db, cache_dir):
-    # Load json files from cache dir
     print("Loading json features")
-    features = []
-    for file in cache_dir.glob('*.json'):
-        with file.open() as f:
-            dhash, metadata_b = json.load(f)
-            for row in db.image_select_by_dhash(bytes.fromhex(dhash)):
-                metadata_a = json.loads(row['metadata'])
-                feature = cmp_metadata(metadata_a, metadata_b)
-                feature['Keep'] = -1 # Keep A (left-side)
-                features.append(feature)
+    deleted = load_json(cache_dir)
 
-            df = pd.DataFrame.from_records(features)
+    print("Loading from database")
+    saved = load_db(db, deleted.index.unique())
 
-    # If no features loaded, predict will just return 0 (equal)
-    if not features:
-        def predict(a,b):
-            return 0
-        return predict
+    print("Fitting data")
+    pipeline = get_pipeline(*analyze_columns(deleted, saved))
+    X, y = join(deleted, saved)
+    pipeline.fit(X, y)
 
-    # Create a mirror set of features (ie. cmp(a,b) and cmp(b,a))
-    df = pd.DataFrame.from_records(features)
-    df = df.append(df * -1)
-    df = df.fillna(0)
-    
-    X = df.drop('Keep', axis=1)
-    y = df['Keep']
-
-    print("Fitting model")
-    model = RandomForestClassifier()
-    model.fit(X, y)
-
-    columns = list(X.columns)
-    print("...done")
-
-    def predict(metadata_a, metadata_b):
-        comparison = cmp_metadata(metadata_a, metadata_b)
-        feature = {
-            col: comparison.get(col, 0)
-            for col in columns
-        }
-
-        df = pd.DataFrame.from_records([feature])
-        prediction = model.predict(df)
-        print(metadata_a['SourceFile'], metadata_b['SourceFile'], prediction)
+    def predict(a, b):
+        a_df = pd.DataFrame.from_records(a, index=[0])
+        b_df = pd.DataFrame.from_records(b, index=[0])
+        X, _ = join(a_df, b_df)
+        prediction = pipeline.predict(X.iloc[:1])
+        print(a['SourceFile'], b['SourceFile'], prediction)
         return prediction
     return predict
+
+
+def data_frame_from_records(records):
+    return pd.DataFrame.from_records(
+        [metadata for dhash in records for metadata in records[dhash]],
+        index=[dhash for dhash in records for _ in records[dhash]],
+    )
+
+
+def load_json(cache_dir):
+    features = defaultdict(list)
+    for file in cache_dir.glob('*.json'):
+        with file.open() as f:
+            dhash, metadata = json.load(f)
+            features[dhash].append(metadata)
+    df = data_frame_from_records(features)
+    return df
+
+
+def load_db(db, hashes):
+    features = defaultdict(list)
+    for dhash in hashes:
+        for row in db.image_select_by_dhash(bytes.fromhex(dhash)):
+            metadata = json.loads(row['metadata'])
+            features[dhash].append(metadata)
+    df = data_frame_from_records(features)
+    return df
+
+
+def join(deleted, saved):
+    left_to_right = deleted.join(saved, lsuffix='_l', rsuffix='_r')
+    left_to_right['Keep'] = 1
+    right_to_left = saved.join(deleted, lsuffix='_l', rsuffix='_r')
+    right_to_left['Keep'] = -1
+    joined = left_to_right.append(right_to_left)
+    return joined.drop(['Keep'], axis=1), joined['Keep']
+
+
+def analyze_columns(deleted, saved):
+    numerical_cols = []
+    string_cols = []
+    date_cols = []
+
+    df = deleted.append(saved)
+
+    for cname in df.columns:
+        dtype = df[cname].dtype
+        pair = (cname + '_l', cname+'_r')
+        if 'Date' in cname:
+            date_cols.extend(pair)
+        elif dtype in ['int64', 'float64']:
+            numerical_cols.extend(pair)
+        elif dtype == "object":
+            string_cols.extend(pair)
+        else:
+            assert False
+    return numerical_cols, string_cols, date_cols
+
+
+def get_pipeline(numerical_cols, string_cols, date_cols):
+    def to_datetime_value(df):
+        """Convert Datetime objects to seconds for numerical/quantitative parsing"""
+        return df.astype(str).apply(
+            lambda x: pd.to_datetime(x.str.slice(
+                0, 19), format='%Y:%m:%d %H:%M:%S').apply(lambda d: d.value)
+        )
+
+    def tokenize(df):
+        tokenizer = CountVectorizer().build_tokenizer()
+
+        def f(x):
+            return set(tokenizer(x))
+        return df.fillna('').applymap(f)
+
+    numerical_col_transformer = ColumnTransformer([
+        ('date', FunctionTransformer(to_datetime_value), date_cols),
+        ('numerical', 'passthrough', numerical_cols),
+    ])
+
+    def subtractor(a):
+        # https://stackoverflow.com/questions/10198747/how-can-i-simultaneously-select-all-odd-rows-and-all-even-columns-of-an-array
+        l = a[:, 1::2]
+        r = a[:, ::2]
+        return l - r
+
+    numerical_pipeline = Pipeline([
+        ('reindexer', ReindexTransformer(numerical_cols + date_cols)),
+        ('numerical_col_transformer', numerical_col_transformer),
+        # TODO can we do better here?
+        ('imputer', SimpleImputer(strategy='constant')),
+        ('subtractor', FunctionTransformer(subtractor)),
+    ])
+
+    string_col_transformer = ColumnTransformer([
+        ('string', FunctionTransformer(tokenize), string_cols),
+    ])
+
+    def build_corpus(a):
+        return [' '.join(set().union(*r)) for r in a]
+
+    string_pipeline = Pipeline([
+        ('reindexer', ReindexTransformer(string_cols)),
+        ('string_col_transformer', string_col_transformer),
+        ('build_corpus', FunctionTransformer(build_corpus)),
+        ('count_vectorizer', CountVectorizer())
+    ])
+
+    feature_union = FeatureUnion([
+        ('numerical_pipeline', numerical_pipeline),
+        ('string_pipeline', string_pipeline)
+    ])
+
+    return Pipeline([
+        ('feature_union', feature_union),
+        ('model', RandomForestClassifier())
+    ])
+
+
+class ReindexTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return X.reindex(columns=self.columns)
