d29bed62421bd7fe6482ecdaf656187b3f7da313
Ben Gerdemann <github@ibeni.net>
2021-02-03 15:22:49 +0000

    Refactor TokenizerTransformer

--- learn/transformers.py
+++ learn/transformers.py
@@ -1,8 +1,7 @@
 from learn.utils import split
 import numpy as np
-import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.feature_extraction.text import CountVectorizer
+
 
 class BaseReindexTransformer(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
@@ -27,27 +26,27 @@ class StringReindexTransformer(BaseReindexTransformer):
         return self
 
 
-
-
 class TokenizerTransformer(BaseEstimator, TransformerMixin):
     def __init__(self, vectorizer):
         self.vectorizer = vectorizer
 
     @staticmethod
-    def str_join_cols(df):
-        return df.astype(str).fillna('').apply(' '.join, axis=1)
-
-    def fit(self, X, y=None):
-        l, _ = split(X)
-        corpus = self.str_join_cols(l)
-        self.vectorizer.fit(corpus)
-        return self
-    
-    def transform(self, X, y=None):
+    def _corpus(X):
         l, r = split(X)
-        l_corpus = self.str_join_cols(l)
-        r_corpus = self.str_join_cols(r)
-        l_transform = self.vectorizer.transform(l_corpus)        
+        l_corpus = l.astype(str).fillna('').apply(' '.join, axis=1)
+        r_corpus = r.astype(str).fillna('').apply(' '.join, axis=1)
+        return l_corpus, r_corpus
+
+    def _transform(self, l_corpus, r_corpus):
+        l_transform = self.vectorizer.transform(l_corpus)
         r_transform = self.vectorizer.transform(r_corpus)
-        c = (l_transform - r_transform).sign()
-        return c
+        return (l_transform - r_transform).sign()
+
+    def fit_transform(self, X, y=None, **fit_params):
+        l_corpus, r_corpus = self._corpus(X)
+        self.vectorizer.fit(l_corpus)
+        return self._transform(l_corpus, r_corpus)
+
+    def transform(self, X, y=None):
+        l_corpus, r_corpus = self._corpus(X)
+        return self._transform(l_corpus, r_corpus)
